74bc37898ca74b75ea0342328360d4a9e207ac19
장준희 <junheejang@wemakeprice.com>
2021-08-05 23:07:24 +0900

    Initial commit

---
+++ src/main/java/com/example/reactivepractice/ReactivePracticeApplication.java
@@ -0,0 +1,11 @@
+package com.example.reactivepractice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivePracticeApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
