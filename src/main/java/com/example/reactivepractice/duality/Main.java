9d309c77dfe264063575c5315b01b905bb87eed7
장준희 <junheejang@wemakeprice.com>
2021-08-05 23:41:46 +0900

    Duality 쌍대성 - Solution

--- src/main/java/com/example/reactivepractice/duality/Main.java
+++ src/main/java/com/example/reactivepractice/duality/Main.java
@@ -1,7 +1,60 @@
 package com.example.reactivepractice.duality;
 
+import java.util.Iterator;
+import java.util.Observable;
+import java.util.Observer;
+
 public class Main {
     public static void main(String[] args) {
-        
+        //pull();
+        push();
+    }
+
+
+    static void pull() {
+        Iterable<Integer> intIterable = () -> new Iterator<Integer>() {
+
+            int i = 0;
+            static final int MAX = 10;
+
+            @Override
+            public boolean hasNext() {
+                return i < MAX;
+            }
+
+            @Override
+            public Integer next() {
+                return ++i;
+            }
+        };
+
+        Iterator<Integer> it = intIterable.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+
+    static void push() {
+        Observer observer = new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                System.out.println(arg);
+            }
+        };
+
+        IntObservable observable = new IntObservable();
+        observable.addObserver(observer);
+        observable.run();
+    }
+
+
+    static class IntObservable extends Observable implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 1; i <= 10; i++) {
+                setChanged();
+                notifyObservers(i);
+            }
+        }
     }
 }
